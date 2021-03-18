d1446e1da95c88a070bac644d900ed78f6b2dc1f
Yoann <ybuzenet@gmail.com>
2021-03-18 15:10:17 +0100

    sequelize init

---
+++ config/config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const result = require("dotenv").config({
+  path: path.resolve(process.cwd(), "./.env.local"),
+});
+if (result.error) {
+  throw result.error;
+}
+module.exports = {
+  development: {
+    username: "root",
+    password: null,
+    database: "database_development",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
