992e30d48b8d0c19a8a740c755c9fa5856d63d83
Yoann <ybuzenet@gmail.com>
2021-04-14 16:39:31 +0200

    wrong variable was used in user route

--- routes/users.js
+++ routes/users.js
@@ -361,9 +361,9 @@ module.exports = function (fastify, opts, done) {
         totalConsumptionThisMonth[0].dataValues.totalAmount;
 
       // Removing properties we don't want to see on Front-End
-      delete userToReturn.dataValues.temporarySecret;
-      delete userToReturn.dataValues.temporaryLastProductPaid;
-      delete userToReturn.dataValues.nonce;
+      delete userToFind.dataValues.temporarySecret;
+      delete userToFind.dataValues.temporaryLastProductPaid;
+      delete userToFind.dataValues.nonce;
 
       reply.send(userToFind);
       return;
