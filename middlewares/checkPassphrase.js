3885ed8242afefbae4c64690297f3f7c30ec9c0d
Yoann <ybuzenet@gmail.com>
2021-05-02 12:50:31 +0200

    middleware now checks header from request instead of payload

--- middlewares/checkPassphrase.js
+++ middlewares/checkPassphrase.js
@@ -1,9 +1,9 @@
 function middlewarePassPhraseCheck(fastify) {
   fastify.addHook("preValidation", async (request, reply) => {
-    console.log("hook has been called");
-    console.log("request", request);
-
-    if (request?.body?.passphrase !== process.env.FRONT_APP_PASSPHRASE) {
+    if (
+      request?.headers?.authorization !==
+      `Bearer ${process.env.FRONT_APP_PASSPHRASE}`
+    ) {
       reply.code(406).send("Passphrase doesn't match.");
       return;
     }
